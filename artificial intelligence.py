b8f9fea9113ccaecd839239e6a806913442c6b4e
library <me@you.com>
2017-09-23 11:38:12 +0000

    some work, web thing

--- artificial intelligence.py
+++ artificial intelligence.py
@@ -2,8 +2,15 @@
 question = input('ask a question ')
 
 def tell_answer():
-		if question == 'How are you?':
-			print("I'm alright, how are you?")
+		if question == input('How are you?'):
+			How_are_you_qn = input("I'm alright,how are you? ")
+			if How_are_you_qn == 'Good':
+				print('Good')
+			elif How_are_you_qn == 'bad':
+				print('Oh no!')
+			else:
+				print("I don't Know what you mean. Is your grammar correct? if it is, my brain doesn't understand.")
+				
 		elif question == 'Do you like coding?':
 			print("Yes, I love coding, it's the best thing in the world!")
 		else:
