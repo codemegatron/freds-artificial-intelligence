2f2b164998a95e797ab0b70deeac7ba48c55a622
codemegatron <32012157+codemegatron@users.noreply.github.com>
2021-11-02 20:51:34 +0000

    fixed function tellAnswer

--- artificial intelligence.py
+++ artificial intelligence.py
@@ -1,7 +1,7 @@
 # python artificial intelligence
 question = input('ask a question ')
 
-def tell_answer():
+def tell_answer(question):
 		if question == input('How are you?'):
 			How_are_you_qn = input("I'm alright,how are you? ")
 			if How_are_you_qn == 'Good':
@@ -15,5 +15,5 @@ def tell_answer():
 			print("Yes, I love coding, it's the best thing in the world!")
 		else:
 			print("I don't Know what you mean. Is your grammar correct? if it is, my brain doesn't understand.")
-tell_answer()
+tell_answer(question)
 	
